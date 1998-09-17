9b04fad0e267ff7c07ef46e727d8fe7fa9c3331e
tell <tell@9168a026-1b44-0410-911b-d774ca5fe8d3>
1998-09-17 18:28:22 +0000

    Added pulldown menus, removed redundant toolbar buttons. Added support for multiple files. Finally got y-axis labels right-justfied by putting them in hboxes.

--- src/wavewin.c
+++ src/wavewin.c
@@ -20,6 +20,9 @@
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  1998/09/01 21:27:24  tell
+ * Initial revision
+ *
  */
 
 #include <ctype.h>
@@ -32,11 +35,133 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/time.h>
-
 #include <gtk/gtk.h>
-
 #include "reader.h"
 #include "gwave.h"
+
+GtkWidget *var_list_submenu;
+
+/* Create a (sub)menu and the item that activates it.
+ * Returns GtkMenu widget pointer.
+ */
+GtkWidget *
+create_menu(char *label, GtkWidget *parent)
+{
+	GtkWidget *item, *menu;
+	
+	if(label)
+		item = gtk_menu_item_new_with_label(label);
+	else
+		item = gtk_menu_item_new();
+
+	menu = gtk_menu_new();
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(item), menu);
+
+	if(parent) {
+		if(GTK_IS_MENU_BAR(parent))
+			gtk_menu_bar_append (GTK_MENU_BAR (parent), item);
+		else if(GTK_IS_MENU(parent))
+			gtk_menu_append(GTK_MENU(parent), item);
+	}
+	gtk_widget_show(item);
+	return menu;
+}
+
+/*
+ * helper function for making menu items.  Returns menu item widget pointer,
+ * but it can often be ignored, since the item is already added to the parent.
+ */
+GtkWidget *
+create_menuitem(char *label, GtkWidget *parent, GtkSignalFunc action, 
+		gpointer p)
+{
+	GtkWidget *item;
+	if(label)
+		item = gtk_menu_item_new_with_label(label);
+	else
+		item = gtk_menu_item_new();
+
+	if(action)
+		gtk_signal_connect (GTK_OBJECT (item), "activate", action, p);
+	if(parent)
+		gtk_menu_append (GTK_MENU(parent), item);
+	gtk_widget_show (item);
+	return item;
+}
+
+/*
+ * create a submenu item for a waveform file that opens/closes the
+ * corresponding variable list window.
+ * Called by g_list_foreach() out of create_gwave_menu().
+ */
+void
+create_wdata_submenuitem(GWDataFile *wdata, GtkWidget *submenu)
+{
+	char buf[128];
+
+	sprintf(buf, "%s: %.120s", wdata->ftag, wdata->df->filename);
+	wdata->menu_item = gtk_menu_item_new_with_label(buf);
+	gtk_menu_append(GTK_MENU(submenu), wdata->menu_item);
+	gtk_signal_connect (GTK_OBJECT (wdata->menu_item), "activate",
+			    GTK_SIGNAL_FUNC(cmd_show_wave_list), wdata);
+	gtk_widget_show(wdata->menu_item);
+}
+
+/*
+ * Create menu bar for main gwave window.
+ */
+static GtkWidget *
+create_gwave_menu()
+{
+	GtkWidget *menubar;
+	GtkWidget *menu;
+       
+	menubar = gtk_menu_bar_new();
+	gtk_widget_show(menubar);
+
+	menu = create_menu("File", menubar);
+	create_menuitem("Read File...", menu, 
+			GTK_SIGNAL_FUNC(get_fname_load_file), NULL);
+	create_menuitem(NULL, menu, NULL, NULL); /* separator */
+	create_menuitem("Quit", menu, GTK_SIGNAL_FUNC(destroy_handler), NULL);
+	
+	menu = create_menu("View", menubar);
+	create_menuitem("Zoom Full", menu, 
+			GTK_SIGNAL_FUNC(cmd_zoom_full), NULL);
+	var_list_submenu = create_menu("Variable List", menu);
+
+	g_list_foreach(wdata_list, 
+		       (GFunc)create_wdata_submenuitem, var_list_submenu);
+
+	return menubar;
+}
+
+/* generate wave button label string, for both initial setup and updating.
+ */
+void
+vw_get_label_string(char *buf, int buflen, VisibleWave *vw)
+{
+	GWDataFile *gdf;
+	double xval, dval;
+	int n, l;
+
+	gdf = (GWDataFile *)vw->var->udata;
+	g_assert(gdf != NULL);
+
+	l = buflen - strlen(gdf->ftag) - 10;
+	n = MIN(l, 15);
+	xval = wtable->cursor[0]->xval;
+	if(vw->var->iv->d.min <= xval && xval <= vw->var->iv->d.max) {
+		dval = an_interp_value(vw->var, xval);
+		sprintf(buf, "%s: %.*s %.3f",
+			gdf->ftag, l, vw->var->d.name, dval);
+	} else {
+		/* should keep track of label state (name vs. name+val)
+		 * and only re-do this if necessary */
+		sprintf(buf, "%s: %.*s    ", gdf->ftag, l, vw->var->d.name);
+	}
+
+}
 
 /*
  * vw_wp_create_button -- called from g_list_foreach to 
@@ -45,14 +170,9 @@
 void
 vw_wp_create_button(VisibleWave *vw, WavePanel *wp)
 {
-	char lbuf[128];
+	char lbuf[64];
 
-	if(wtable->cursor[0]->shown) {
-		double dval = an_interp_value(vw->var, wtable->cursor[0]->xval);
-		sprintf(lbuf, "%.15s %.3f", vw->var->d.name, dval);
-	} else {
-		sprintf(lbuf, "%.15s      ", vw->var->d.name);
-	}
+	vw_get_label_string(lbuf, 64, vw);
 	vw->label = gtk_label_new(lbuf);
 	vw->button = gtk_toggle_button_new();
 	gtk_container_add(GTK_CONTAINER(vw->button), vw->label);
@@ -61,6 +181,7 @@ vw_wp_create_button(VisibleWave *vw, WavePanel *wp)
 	sprintf(lbuf, "wavecolor%d", vw->colorn);
 	gtk_widget_set_name(vw->label, lbuf);
 	gtk_widget_show(vw->label);
+	gtk_widget_set_name(vw->button, "wavebutton");
 	gtk_widget_show(vw->button);
 }
 
@@ -70,7 +191,7 @@ vw_wp_create_button(VisibleWave *vw, WavePanel *wp)
 void setup_waveform_window(void)
 {
 	int i;
-	GtkWidget *box1, *hbox, *bbox, *btn;
+	GtkWidget *box0, *box1, *hbox, *bbox, *btn, *menubar;
 	/* some size information. */
 	const int min_w=80, min_h=50, nom_w=600, nom_h=100;
 
@@ -84,11 +205,19 @@ void setup_waveform_window(void)
 	gtk_signal_connect(
 		GTK_OBJECT(win_main), "delete_event",
 		GTK_SIGNAL_FUNC(destroy_handler), NULL);
-	gtk_container_border_width (GTK_CONTAINER (win_main), 10);
+/*	gtk_container_border_width (GTK_CONTAINER (win_main), 10); */
 
 	/* create the vertical box, and add it to the window */
+	box0 = gtk_vbox_new(FALSE, 0);
+	gtk_container_add (GTK_CONTAINER (win_main), box0);
+	gtk_widget_show(box0);
+
+	menubar = create_gwave_menu();
+	gtk_box_pack_start(GTK_BOX(box0), menubar, FALSE, TRUE, 0);
+
 	box1 = gtk_vbox_new(FALSE, 0);
-	gtk_container_add (GTK_CONTAINER (win_main), box1);
+	gtk_container_border_width (GTK_CONTAINER (box1), 5);
+	gtk_container_add (GTK_CONTAINER (box0), box1);
 
 	/* create horizontal button box, add to top, put some buttons in it.
 	* If we add ANY more commands, need to go to pulldown menus. */
@@ -96,12 +225,6 @@ void setup_waveform_window(void)
 	gtk_button_box_set_layout(GTK_BUTTON_BOX(bbox), GTK_BUTTONBOX_START);
 	gtk_button_box_set_spacing(GTK_BUTTON_BOX(bbox), 5);
 	gtk_box_pack_start(GTK_BOX(box1), bbox, FALSE, FALSE, 0);
-
-	btn = gtk_button_new_with_label ("Quit");
-	gtk_container_add (GTK_CONTAINER(bbox), btn);
-	gtk_signal_connect (GTK_OBJECT (btn), "clicked",
-			    GTK_SIGNAL_FUNC(destroy_handler), NULL);
-	gtk_widget_show (btn);
 
 	btn = gtk_button_new_with_label ("Zoom In");
 	gtk_container_add (GTK_CONTAINER(bbox), btn);
@@ -115,23 +238,12 @@ void setup_waveform_window(void)
 			    GTK_SIGNAL_FUNC(cmd_zoom_out), NULL);
 	gtk_widget_show (btn);
 
-	btn = gtk_button_new_with_label ("Zoom Full");
-	gtk_container_add (GTK_CONTAINER(bbox), btn);
-	gtk_signal_connect (GTK_OBJECT (btn), "clicked",
-			    GTK_SIGNAL_FUNC(cmd_zoom_full), NULL);
-	gtk_widget_show (btn);
-
 	btn = gtk_button_new_with_label ("Delete");
 	gtk_container_add (GTK_CONTAINER(bbox), btn);
 	gtk_signal_connect (GTK_OBJECT (btn), "clicked",
 			    GTK_SIGNAL_FUNC(cmd_delete_selected_waves), NULL);
 	gtk_widget_show (btn);
 
-	btn = gtk_button_new_with_label ("Variables");
-	gtk_container_add (GTK_CONTAINER(bbox), btn);
-	gtk_signal_connect (GTK_OBJECT (btn), "clicked",
-			    GTK_SIGNAL_FUNC(cmd_show_wave_list), NULL);
-	gtk_widget_show (btn);
 	gtk_widget_show(bbox);
 
 	/* label with cursor status */
@@ -150,28 +262,31 @@ void setup_waveform_window(void)
 
 		/* y-axis labels and signal names, all in a vbox */
 		wp->lvbox = gtk_vbox_new(FALSE, 0);
+		gtk_widget_set_usize(wp->lvbox, 140, -1);
 		gtk_widget_show(wp->lvbox);
 
 		gtk_table_attach(GTK_TABLE(wtable->table), wp->lvbox, 
 			 0, 1, i, i+1, 
 			 GTK_FILL, GTK_EXPAND|GTK_FILL, 4, 0);
 
-		gtk_signal_connect (GTK_OBJECT (wp->lvbox), 
-			  "drop_data_available_event",
-			  GTK_SIGNAL_FUNC(wavepanel_dnd_drop),
-			  (gpointer)wp);
-
-		/* FIX: really want these labels to be right-justified. */
+		hbox = gtk_hbox_new(FALSE, 0);
 		sprintf(lbuf, "%.3f", wp->max_yval);
 		wp->lab_max = gtk_label_new(lbuf);
-		gtk_box_pack_start(GTK_BOX(wp->lvbox), wp->lab_max,
+		gtk_box_pack_start(GTK_BOX(wp->lvbox), hbox,
 			 FALSE, FALSE, 0);
+		gtk_box_pack_end(GTK_BOX(hbox), wp->lab_max,
+			 FALSE, FALSE, 0);
+		gtk_widget_show(hbox);
 		gtk_widget_show(wp->lab_max);
 
+		hbox = gtk_hbox_new(FALSE, 0);
 		sprintf(lbuf, "%.3f", wp->min_yval);
 		wp->lab_min = gtk_label_new(lbuf);
-		gtk_box_pack_end(GTK_BOX(wp->lvbox), wp->lab_min,
+		gtk_box_pack_end(GTK_BOX(wp->lvbox), hbox,
 			   FALSE, FALSE, 0);
+		gtk_box_pack_end(GTK_BOX(hbox), wp->lab_min,
+			   FALSE, FALSE, 0);
+		gtk_widget_show(hbox);
 		gtk_widget_show(wp->lab_min);
 
 		g_list_foreach(wp->vwlist, (GFunc)vw_wp_create_button, wp);
