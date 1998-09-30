012db87f1d001a3146858fa0603af5a8bb7a0186
tell <tell@9168a026-1b44-0410-911b-d774ca5fe8d3>
1998-09-30 21:58:15 +0000

    Reorganization of mouse-button event handling to support both dragging of cursors and select_x_range primitive used to get the range for cmd_zoom_window. add supress_redraw and some misc stuff while tracking down some bugs. Attempt to make drawing the waveforms a bit more efficient.

--- src/event.c
+++ src/event.c
@@ -1,6 +1,8 @@
 /*
  * event.c, part of the gwave waveform viewer tool
  * Functions for handling low-level events (clicks, drag-and-drop)
+ * Some drawing things are here if they are related to mouse operations;
+ * perhaps they should move.
  *
  * Copyright (C) 1998  University of North Carolina at Chapel Hill
  *
@@ -19,6 +21,11 @@
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  1998/09/17 18:35:49  tell
+ * wrap DnD message as type GWDnDData.
+ * Split cursor draw/update into several functions in preparation for
+ * support of dragging.
+ *
  * Revision 1.1  1998/09/01 21:28:20  tell
  * Initial revision
  *
@@ -74,6 +81,67 @@ vw_wp_visit_update_labels(gpointer p, gpointer d)
 
 	vw_get_label_string(lbuf, 64, vw);
 	gtk_label_set(GTK_LABEL(vw->label), lbuf);
+}
+
+void
+set_all_wp_cursors(int cnum)
+{
+	GdkCursor *cursor;
+	int i;
+	WavePanel *wp;
+
+	if(cnum == -1)
+		cursor = NULL;
+	else
+		cursor = gdk_cursor_new(cnum);
+	for(i = 0; i < wtable->npanels; i++) {
+		wp = &wtable->panels[i];
+		gdk_window_set_cursor(wp->drawing->window, cursor);
+	}
+	if(cursor)
+		gdk_cursor_destroy(cursor);
+}
+
+/*
+ * The next several routines implement the generic operation of 
+ * selecting a subset of the visible part of the X axis by dragging
+ * with button 1.
+ */
+void
+select_x_range(SRFunc func, gpointer data)
+{
+	wtable->srange->done_callback = func;
+	wtable->srange->done_data = data;
+	set_all_wp_cursors(GDK_RIGHT_SIDE);
+	wtable->mstate = M_SELRANGE_ARMED;
+}
+
+/* draw or undraw srange line, using XOR gc */
+void
+draw_srange(SelRange *sr)
+{
+	if(!sr->gc) {
+		gdk_color_alloc(win_colormap, &sr->gdk_color);
+		sr->gc = gdk_gc_new(sr->wp->drawing->window);
+		gdk_gc_set_foreground(sr->gc, &sr->gdk_color);
+		gdk_gc_set_background(sr->gc, &bg_gdk_color);
+		gdk_gc_set_function(sr->gc, GDK_XOR);
+	}
+	g_assert(sr->gc != NULL);
+	gdk_draw_line(sr->wp->drawing->window, sr->gc,
+		      sr->x1, sr->y, sr->x2, sr->y);
+}
+
+void
+update_srange(SelRange *sr, int newx2, int draw)
+{
+	if(sr->drawn)	/* undraw old */
+		draw_srange(sr);
+	sr->drawn = draw;
+	sr->x2 = newx2;
+	if(draw) {	/* draw new if requested */
+		draw_srange(sr);
+	}
 }
 
 /*
@@ -146,35 +214,131 @@ update_cursor(VBCursor *csp, double xval)
 	gtk_container_enable_resize(GTK_CONTAINER(win_main));
 }
 
-/*
- * TODO: implement dragging cursors around instead of this 
- * simple click-to-place stuff.
- */
-gint click_handler(GtkWidget *widget, GdkEventButton *event, 
-			  gpointer data)
+static void
+window_update_cursor(WavePanel *wp, VBCursor *csp, int x)
 {
 	double xval;
+	g_assert(csp != NULL);
+
+	xval = x2val(wp, x);
+	if(fabs(xval - csp->xval) < DBL_EPSILON && csp->shown)
+		return;
+	if(xval < wp->start_xval || xval > wp->end_xval)
+		return;
+	update_cursor(csp, xval);
+}
+
+/*
+ * button_press in any wave panel.
+ */
+gint
+button_press_handler(GtkWidget *widget, GdkEventButton *event, 
+			  gpointer data)
+{
 	WavePanel *wp = (WavePanel *)data;
-	VBCursor *csp;
+	GdkCursor *cursor;
 
 	switch(event->button) {
 	case 1:
-		csp = wtable->cursor[0];
-		break;
 	case 2:
-		csp = wtable->cursor[1];
-		break;
+		switch(wtable->mstate) {
+		case M_NONE:
+			gtk_grab_add(widget);
+			wtable->mstate = M_CURSOR_DRAG;
+			wtable->button_down = event->button;
+
+			cursor = gdk_cursor_new(GDK_SB_H_DOUBLE_ARROW);
+			gdk_window_set_cursor(widget->window, cursor);
+			gdk_cursor_destroy(cursor);
+			wtable->drag_cursor = wtable->cursor[event->button-1];
+			window_update_cursor(wp, wtable->drag_cursor, event->x);
+			break;
+		case M_SELRANGE_ARMED:
+			gtk_grab_add(widget);
+			wtable->button_down = event->button;
+			wtable->mstate = M_SELRANGE_ACTIVE;
+
+			wtable->srange->y = event->y;
+			wtable->srange->x1 = event->x;
+			wtable->srange->wp = wp;
+			break;
+		/* can't start another drag until first one done */
+		case M_CURSOR_DRAG:
+		case M_SELRANGE_ACTIVE:
+			break;
+		default:
+			break;
+			
+		}
 	default:
-		return 0;
+		break;
 	}
-
-	xval = x2val(wp, event->x);
-	if(fabs(xval - csp->xval) < DBL_EPSILON && csp->shown)
-		return 0;	/* clicked in same place */
-
-	update_cursor(csp, xval);
+/*	fprintf(stderr, "P%d;mstate=%d\n", event->button, wtable->mstate); */
 	return 0;
 }
+
+/*
+ * button_release in any wave panel.
+ */
+gint
+button_release_handler(GtkWidget *widget, GdkEventButton *event, 
+			  gpointer data)
+{
+	WavePanel *wp = (WavePanel *)data;
+
+	if(wtable->button_down != event->button)
+		return 0;
+
+	switch(wtable->mstate) {
+	case M_CURSOR_DRAG:
+		gtk_grab_remove(widget);
+		gdk_window_set_cursor(widget->window, NULL);
+		window_update_cursor(wp, wtable->drag_cursor, event->x);
+		wtable->drag_cursor = NULL;
+		break;
+	case M_SELRANGE_ACTIVE:
+		gtk_grab_remove(widget);
+		set_all_wp_cursors(-1);
+		update_srange(wtable->srange, event->x, 0);
+		(wtable->srange->done_callback)(wtable->srange->wp,
+						wtable->srange->x1, 
+						wtable->srange->x2, 
+						wtable->srange->done_data);
+		break;
+	default:
+	}
+	wtable->mstate = M_NONE;
+	wtable->button_down = -1;
+/*	fprintf(stderr, "R%d;mstate=%d\n", event->button, wtable->mstate); */
+	return 0;
+}
+
+/*
+ * GDK_MOTION_NOTIFY in any WavePanel's drawing area
+ */
+gint
+motion_handler(GtkWidget *widget, GdkEventMotion *event, 
+			  gpointer data)
+{
+	WavePanel *wp = (WavePanel *)data;
+	VBCursor *csp;
+
+	switch(wtable->mstate) {
+	case M_CURSOR_DRAG:
+		csp = wtable->drag_cursor;
+		window_update_cursor(wp, csp, event->x);
+		break;
+	case M_SELRANGE_ACTIVE:
+		/* fputc('r', stderr); */
+		update_srange(wtable->srange, event->x, 1);
+		break;
+	default:
+		fputc('.', stderr);
+		break;
+	}
+	return 0;
+}
+
 
 gint scroll_handler(GtkWidget *widget)
 {
@@ -189,12 +353,14 @@ gint scroll_handler(GtkWidget *widget)
 	wtable->end_xval = hsadj->value + owidth;
 
 	draw_labels();
-	for(i = 0; i < wtable->npanels; i++) {
-		wp = &wtable->panels[i];
-		wp->start_xval = wtable->start_xval;
-		wp->end_xval = wtable->end_xval;
-		draw_wavepanel(wp->drawing, NULL, wp);
-	}
+
+	if(wtable->suppress_redraw == 0)
+		for(i = 0; i < wtable->npanels; i++) {
+			wp = &wtable->panels[i];
+			wp->start_xval = wtable->start_xval;
+			wp->end_xval = wtable->end_xval;
+			draw_wavepanel(wp->drawing, NULL, wp);
+		}
 	return 0;
 }
 
@@ -226,27 +392,22 @@ gint expose_handler(GtkWidget *widget, GdkEventExpose *event,
 		alloc_colors(widget);
 		colors_initialized = 1;
 	}
-	/*
-	 * Make sure we've got GCs for each visible wave.
-	 */
-	g_list_foreach(wp->vwlist, (GFunc)vw_wp_setup_gc, wp);
 
-	if ( wp->pixmap ) {
-		/* CHECK/FIX: only need to free/new if size changed? */
+	/* Make sure we've got GCs for each visible wave. */
+/*	g_list_foreach(wp->vwlist, (GFunc)vw_wp_setup_gc, wp); */
+
+	if ( wp->pixmap && (wp->width != w || wp->height != h)) {
 		gdk_pixmap_unref(wp->pixmap);
-	}
-	wp->pixmap = gdk_pixmap_new(widget->window, w, h, -1);
+		wp->width = w;
+		wp->height = h;
+		wp->pixmap = NULL;
 
-	/* since the scrollbar event also redraws everything, 
-	 * let its handler do all the work by sending it the signal
-	 * bonus: gets the scrollbar updated if this event is due to a resize.
-	 */
-	/* now that we have many panels, this might cause multiple 
-	 *  unnecessary redraws.  Try to figure out if this is the case,
-	 *  and how to fix it. 
-	 */
-	gtk_signal_emit_by_name(GTK_OBJECT(win_hsadj), "changed");
-	gtk_signal_emit_by_name(GTK_OBJECT(win_hsadj), "value_changed");
+	}
+	if(!wp->pixmap)
+		wp->pixmap = gdk_pixmap_new(widget->window, w, h, -1);
+
+	if(wtable->suppress_redraw == 0)
+		draw_wavepanel(wp->drawing, event, wp);
 
 	return 0;
 }
