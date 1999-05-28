f89f7e36e4c0e9d132fab325b1d18019d990f474
tell <tell@9168a026-1b44-0410-911b-d774ca5fe8d3>
1999-05-28 23:04:48 +0000

    changes to support using spicefile library to read data files add update_wfile_waves() to support file-reload operation add remove_wfile_waves() to support file-delete operation Try to patch up wavepanel_update_data() and wavetable_update_data() to support file-reload and file-delete cases.

--- src/cmd.c
+++ src/cmd.c
@@ -22,6 +22,9 @@
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  1999/01/08 22:42:13  tell
+ * minor changes to support wavepanel add/delete
+ *
  * Revision 1.1  1998/12/26 04:38:06  tell
  * Initial revision
  *
@@ -53,7 +56,6 @@
 
 #include <gtk/gtk.h>
 
-#include "reader.h"
 #include "gwave.h"
 
 gint cmd_zoom_absolute(double start, double end)
@@ -199,6 +201,7 @@ remove_wave_from_panel(WavePanel *wp, VisibleWave *vw)
 	gtk_widget_destroy(vw->button);
 
 	gdk_gc_destroy(vw->gc);
+	g_free(vw->varname);
 	g_free(vw);
 
 	wavepanel_update_data(wp);
@@ -206,15 +209,111 @@ remove_wave_from_panel(WavePanel *wp, VisibleWave *vw)
 }
 
 /*
- * Add a waveform to a WavePanel
+ * remove_wfile_waves - 
+ * Remove from their respective panels all waveforms from the specified file.
+ * This works just like delete_selected_waves, except for the test
+ * that puts them on the to-delete list.  
+ * TODO: factor out this common code.
+ */
+
+struct wp_file_pkg {
+	WavePanel *wp;
+	GWDataFile *gdf;
+};
+
+void vw_list_if_wfile(gpointer p, gpointer d)
+{
+	VisibleWave *vw = (VisibleWave *)p;
+	struct wp_file_pkg *foo = (struct wp_file_pkg *)d;
+
+	if(vw->gdf == foo->gdf) {
+		VWListItem *vdi = g_new(VWListItem, 1);
+		vdi->wp = foo->wp;
+		vdi->vw = vw;
+		vw_delete_list = g_list_append(vw_delete_list, vdi);
+	}
+}
+
+void
+remove_wfile_waves(GWDataFile *wdata)
+{
+	int i;
+	VWListItem *vdi;
+	struct wp_file_pkg foo;
+	foo.gdf = wdata;
+
+	for(i = 0; i < wtable->npanels; i++) {
+		WavePanel *wp = wtable->panels[i];
+		foo.wp = wp;
+		g_list_foreach(wp->vwlist, vw_list_if_wfile, &foo);
+	}
+	while((vdi = g_list_nth_data(vw_delete_list, 0)) != NULL) {
+		/*printf("tearing down %s from %s\n", vdi->vw->varname, wdata->wf->wf_filename); */
+		remove_wave_from_panel(vdi->wp, vdi->vw);
+		vw_delete_list = g_list_remove(vw_delete_list, vdi);
+		g_free(vdi);
+	}
+}
+
+/*
+ * Repoint all visible waves that reference this file to the new data.   
+ * Any visiblewaves referencing variables that no longer exist in the new file
+ * are deleted.
  */
 void
-add_var_to_panel(WavePanel *wp, DVar *dv)
+update_wfile_waves(GWDataFile *wdata)
+{
+	int i;
+	VWListItem *vdi;
+	struct wp_file_pkg foo;
+	WaveVar  *wv;
+	int foundone = 0;
+
+	foo.gdf = wdata;
+
+	/* get a list of all VisibleWaves referencing this file */
+	for(i = 0; i < wtable->npanels; i++) {
+		WavePanel *wp = wtable->panels[i];
+		foo.wp = wp;
+		g_list_foreach(wp->vwlist, vw_list_if_wfile, &foo);
+	}
+	while((vdi = g_list_nth_data(vw_delete_list, 0)) != NULL) {
+		foundone = 1;
+		wv = wf_find_variable(wdata->wf, vdi->vw->varname);
+		if(wv) {
+			/* printf("updated variable %s to %lx\n", vdi->vw->varname, wdata->wf); */
+			vdi->vw->gdf = wdata;
+			vdi->vw->var = wv;
+		} else {
+			/* printf("variable %s no longer in file %s; removing from panel\n", vdi->vw->varname, wdata->wf->wf_filename); */
+			remove_wave_from_panel(vdi->wp, vdi->vw);
+		}
+
+		vw_delete_list = g_list_remove(vw_delete_list, vdi);
+		g_free(vdi);
+	}
+
+	if(foundone) {
+		for(i = 0; i < wtable->npanels; i++) {
+			WavePanel *wp = wtable->panels[i];
+			wavepanel_update_data(wp);
+		}
+		wavetable_update_data();
+	}
+}
+
+/*
+ * Add a new waveform to a WavePanel
+ */
+void
+add_var_to_panel(WavePanel *wp, WaveVar *dv)
 {
 	VisibleWave *vw;
 
 	vw = g_new0(VisibleWave, 1);
 	vw->var = dv;
+	vw->varname = g_strdup(dv->wv_name);
+	vw->gdf = (GWDataFile *)dv->udata;
 	vw->colorn = wp->nextcolor;
 	wp->nextcolor = (wp->nextcolor + 1)%NWColors;
 
@@ -240,16 +339,21 @@ vw_wp_visit_update_data(gpointer p, gpointer d)
 	VisibleWave *vw = (VisibleWave *)p;
 	WavePanel *wp = (WavePanel *)d;
 
-	if(vw->var->iv->d.min < wp->min_xval)
-		wp->min_xval = vw->var->iv->d.min;
-	if(vw->var->iv->d.max > wp->max_xval)
-		wp->max_xval = vw->var->iv->d.max;
+	if(vw->var->wv_iv->wds->min < wp->min_xval)
+		wp->min_xval = vw->var->wv_iv->wds->min;
+	if(vw->var->wv_iv->wds->max > wp->max_xval)
+		wp->max_xval = vw->var->wv_iv->wds->max;
 
-	if(vw->var->d.min < wp->min_yval)
-		wp->min_yval = vw->var->d.min;
-	if(vw->var->d.max > wp->max_yval)
-		wp->max_yval = vw->var->d.max;
+	if(vw->var->wds[0].min < wp->min_yval)
+		wp->min_yval = vw->var->wds[0].min;
+	if(vw->var->wds[0].max > wp->max_yval)
+		wp->max_yval = vw->var->wds[0].max;
 }
+
+/* FIXME:sgt: wavepanel_update_data and wavetable_update_data
+ * need a rethink and rewrite; they still don't do the right
+ * thing in all cases.
+ */
 
 /*
  * wavepanel_update_data
@@ -271,9 +375,9 @@ wavepanel_update_data(WavePanel *wp)
 	 * like if the panel was empty
 	 */
 	if(wp->min_xval == G_MAXDOUBLE)
-		wp->min_xval = wtable->min_xval;
+		wp->min_xval = 0; /* wtable->min_xval; */
 	if(wp->max_xval == G_MINDOUBLE)
-		wp->max_xval = wtable->max_xval;
+		wp->max_xval = 0; /* wtable->max_xval; */
 	if(wp->min_yval == G_MAXDOUBLE)
 		wp->min_yval = 0.0;
 	if(wp->max_yval == G_MINDOUBLE)
@@ -292,6 +396,10 @@ wavepanel_update_data(WavePanel *wp)
 		wp->start_xval = wp->min_xval;
 		wp->end_xval = wp->max_xval;
 	}
+	if(wp->start_xval < wp->min_xval)
+		wp->start_xval = wp->min_xval;
+	if(wp->end_xval > wp->max_xval)
+		wp->end_xval = wp->max_xval;
 
 	/* Update y-axis labels */
 	if(wp->lab_min) {
@@ -334,11 +442,13 @@ wavetable_update_data()
 	if(wtable->max_xval == G_MINDOUBLE)
 		wtable->max_xval = 0;
 
-	/* if start & end were the same, try updating them so we can
-	* see somthing.
+	/* if start & end were the same or out of range, 
+	* just zoom-full so we can see somthing.
 	*/
-	if(fabs(wtable->end_xval - wtable->start_xval) < DBL_EPSILON &&
-		win_hsadj != NULL) {
+	if((fabs(wtable->end_xval - wtable->start_xval) < DBL_EPSILON
+	    || wtable->start_xval < wtable->min_xval
+	    || wtable->end_xval > wtable->max_xval)
+	   && win_hsadj != NULL) {
 		wtable->suppress_redraw = 1;
 		cmd_zoom_full(NULL);
 		wtable->suppress_redraw = 0;
@@ -347,10 +457,20 @@ wavetable_update_data()
 
 		/* min/max changed, might have added first (or removed last)
 		 * wave from a file with different range.
-		 * Keep start/end the same, but update scrollbar.
+		 * try to keep start/end same, but make them sane if needed.
+		 * then update scrollbar.
 		 */
+		if(wtable->start_xval < wtable->min_xval)
+			wtable->start_xval = wtable->min_xval;
+		if(wtable->end_xval > wtable->max_xval)
+			wtable->end_xval = wtable->max_xval;
+		win_hsadj->page_size = fabs(wtable->end_xval - wtable->start_xval);
+		win_hsadj->page_increment = win_hsadj->page_size/2;
+		win_hsadj->step_increment = win_hsadj->page_size/100;
+		win_hsadj->value = wtable->start_xval;
 		win_hsadj->lower = wtable->min_xval;
 		win_hsadj->upper = wtable->max_xval;
+
 		wtable->suppress_redraw = 1;
 		gtk_signal_emit_by_name(GTK_OBJECT(win_hsadj), "value_changed");
 		wtable->suppress_redraw = 0;
