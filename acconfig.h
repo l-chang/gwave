4c6f7cdccb2263d84c74f93b517df3f023d5590f
tell <tell@9168a026-1b44-0410-911b-d774ca5fe8d3>
2000-01-07 06:33:03 +0000

    Copied in latest NEWS, which still isn't up to date. Merged in the guile and guile-gtk stuff

--- acconfig.h
+++ acconfig.h
@@ -26,3 +26,67 @@
  */
 #undef PROG_GRAPH
 
+/* Define this if your libguile has a scm_eval_string that is safe against
+   re-entry by continuations. This should be true of snapshots newer than
+   970928.  */
+#undef HAVE_SAFE_SCM_EVAL_STRING
+
+/* Define this if your libguile exports scm_puts, meaning that
+   scm_gen_puts should no longer be used. This should be true of
+   snapshots newer than 971014.  */
+#undef HAVE_SCM_PUTS
+
+/* Define this if your libguile has gh_vector_ref instead of gh_vref,
+   meaning that gh_vref should no longer be used. This should be
+   true of snapshots newer than 971012.  */
+#undef HAVE_GH_VECTOR_REF
+
+/* Define this if your libguile has gh_vector_set_x instead of gh_vset,
+   meaning that gh_vset should no longer be used. This should be
+   true of snapshots newer than 971020.  */
+#undef HAVE_GH_VECTOR_SET_X
+
+/* Define this if your libguile has readline support. This should be
+   true of snapshots newer than 971023.  */
+#undef HAVE_SCM_READLINE
+
+/* Define this if your libguile has gh_length and not
+   gh_list_length. This should be true of snapshots newer than 970915.  */
+#undef HAVE_GH_LENGTH
+
+/* Define this if your libguile has scm_parse_path.  */
+#undef HAVE_SCM_PARSE_PATH
+
+/* Define this if your libguile has scm_internal_select.  */
+#undef HAVE_SCM_INTERNAL_SELECT
+
+/* Define this if your libguile has scm_the_last_stack_fluid instead
+   of scm_the_last_stack_var.  */
+#undef HAVE_SCM_THE_LAST_STACK_FLUID
+
+/* Define this if your libguile has scm_internal_cwdr.  */
+#undef HAVE_SCM_INTERNAL_CWDR
+
+/* Define this if your libguile has scm_internal_stack_catch.  */
+#undef HAVE_SCM_INTERNAL_STACK_CATCH
+
+/* Define this if your libguile has scm_internal_parse_path,
+   which should be used instead of scm_parse_path from C.  */
+#undef HAVE_SCM_INTERNAL_PARSE_PATH
+
+/* Define this if your libguile has a scm_make_vector, which needs
+   three arguments. This should be true only of older versions. */
+#undef HAVE_SCM_MAKE_VECTOR_3_ARGS
+
+/* Define this if your libguile has scm_load_startup_files,
+   which means the hack to get boot-9.scm to be loaded is unnecessary
+   and even dangerous. */
+#undef HAVE_SCM_LOAD_STARTUP_FILES
+
+/* Define this if your libguile has scm_make_hook, indicating
+   C-level support for hooks. */
+#undef HAVE_SCM_MAKE_HOOK
+
+/* Define this if your libguile has scm_strport_to_string
+   (added sometime after guile-1.3) */
+#undef HAVE_SCM_STRPORT_TO_STRING
