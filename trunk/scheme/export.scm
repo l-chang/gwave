154ef132631b3e71fbcb241c24a15b4ca8e5d31a
sgt <sgt@9168a026-1b44-0410-911b-d774ca5fe8d3>
2003-06-06 05:33:37 +0000

    export-gnugraph working, and added to main and panel menus still needs work. todo: add extensible scheme for passing options to plot procedures like export-wavepanels-gnugraph, add notebook of plot-filter-specific options, add extensible scheme for registering different plot backends.

--- trunk/scheme/export.scm
+++ trunk/scheme/export.scm
@@ -6,6 +6,7 @@
   :use-module (gtk gtk)
   :use-module (ice-9 optargs)
   :use-module (app gwave cmds)
+  :use-module (app gwave export-gnugraph)
 )
 (read-set! keywords 'prefix)
 
@@ -144,3 +145,131 @@
     (gtk-widget-grab-default export-btn)
     (gtk-widget-show window)
 ))
+
+
+(define-public (popup-plot-dialog plist)
+  (let* ((window (gtk-window-new 'toplevel))
+	 (vbox (gtk-vbox-new #f 0))
+	 (hbox1 (gtk-hbox-new #f 10))
+	 (outf-label (gtk-label-new "Output File:"))
+	 (filename-entry (gtk-entry-new))
+	 (browse-btn (gtk-button-new-with-label "Browse"))
+
+	 (plot-procedure #f)
+	 (plot-options '())
+	 (hbox2 (gtk-hbox-new #f 10))
+
+	 (action-hbox (gtk-hbox-new #f 10))
+	 (separator (gtk-hseparator-new))
+	 (cancel-btn (gtk-button-new-with-label "Cancel"))
+	 (export-btn (gtk-button-new-with-label "Plot"))
+	 )
+    
+    (gtk-window-set-title window "Plot Data")
+    (gtk-container-border-width window 0)
+    (gtk-container-add window vbox)
+    (gtk-widget-show vbox)
+
+    (gtk-container-border-width hbox1 10)
+    (gtk-box-pack-start vbox hbox1 #f #t 0)
+    (gtk-widget-show hbox1)
+
+    (gtk-box-pack-start hbox1 outf-label #f #t 0)
+    (gtk-widget-show outf-label)
+
+    (gtk-box-pack-start hbox1 filename-entry #f #t 0)
+    (gtk-widget-show filename-entry)
+    (gtk-entry-set-text filename-entry "gwplot.dat")
+
+    (gtk-box-pack-start hbox1 browse-btn #f #t 0)
+    (gtk-signal-connect browse-btn "clicked"
+			(lambda ()
+			  (with-selected-filename 
+			   "Plot to file"
+			   (lambda (f)
+			     (gtk-entry-set-text filename-entry f))
+			   #:default (gtk-entry-get-text filename-entry))))
+    (gtk-widget-show browse-btn)
+
+    ; TODO: notebook with entry for each supported plot filter
+    ; containing that filter's various options
+    (set! plot-procedure export-wavepanels-gnugraph)
+    (set! plot-options (list "ps"))
+
+    ; row of action buttons
+    (gtk-container-border-width action-hbox 10)
+    (gtk-box-pack-start vbox action-hbox #f #t 0)
+    (gtk-widget-show action-hbox)
+
+    (gtk-signal-connect cancel-btn "clicked" 
+			(lambda () 
+			  (gtk-widget-destroy window)))
+    (gtk-box-pack-start action-hbox cancel-btn #t #t 0)
+    (gtk-tooltips-set-tip gwave-tooltips cancel-btn
+			  "Cancel export and close window" "")
+    (gtk-widget-show cancel-btn)
+
+    (gtk-box-pack-start action-hbox export-btn #t #t 0)
+    (gtk-tooltips-set-tip gwave-tooltips export-btn "Plot data" "")
+    (gtk-signal-connect export-btn "clicked" 
+			(lambda ()
+			  (if plot-procedure
+			      (plot-procedure 
+			       (gtk-entry-get-text filename-entry) 
+			       plist 
+			       plot-options))
+			  (gtk-widget-destroy window)))
+    (gtk-widget-show export-btn)
+    
+    (gtk-widget-set-flags export-btn '(can-default))
+    (gtk-widget-grab-default export-btn)
+    (gtk-widget-show window)
+))
+
+
+; run a command in a subprocess, redirecting its output to a named file.
+(define-public (subprocess-to-file f cmd arglist)
+  (let ((port (open f (logior O_WRONLY O_CREAT O_TRUNC) #o0777))
+	(null (open "/dev/null" O_RDONLY 0)))
+    (format #t "subprocess-to-file ~s\n" arglist)
+    (flush-all-ports)
+    ; TODO: stat cmd to make sure its executable.
+    (let ((p (primitive-fork)))
+      (cond ((< p 0)
+	     ; error
+	     (error "fork"))
+	    ((eq? 0 p)
+	     ; child
+	     (redirect-port port (current-output-port))
+	     (redirect-port null (current-input-port))
+	     (close-all-ports-except (current-input-port) 
+				     (current-output-port) 
+				     (current-error-port))
+	     (apply execlp cmd arglist)
+	     )
+	    (else 
+	     ; parent
+	     (close port)
+	     (format #t "pid is ~d\n" p)
+	     )))))
+
+(define (reap-child)
+  (let* ((w (catch 'system-error
+	    (lambda () (waitpid 0 WNOHANG))
+	    (lambda (func . stuff) (cons 0 #f))))
+	 (pid (car w))
+	 (st (cdr w)))
+    (if (not (eq? 0 pid))
+	(begin
+	  (format #t "process ~d" pid)
+	  (if (status:exit-val st)
+	      (format #t " exited (~d)" (status:exit-val st)))
+	  (if (status:term-sig st)
+	      (format #t " terminated on signal ~d" (status:term-sig st)))
+	  (if (status:stop-sig st)
+	      (format #t " stopped on signal~d" (status:exit-sig st)))
+	  (display "\n"))
+	(display "no child\n"))))
+
+(dbprint "setting SIGCHLD handler")
+(sigaction SIGCHLD (lambda (s) (reap-child)))
