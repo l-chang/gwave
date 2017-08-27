bb3c7946754dd1c32e3ac8d6d96c5d7eff3aeec9
l-chang <l-chang@users.noreply.github.com>
2017-08-27 12:47:05 +0800

    Updated autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,20 +1,6 @@
+#!/bin/sh
+
 ## autogen.sh - generate all the twisty little files.
 
-# Generate aclocal.m4 from configure.in
-aclocal
-
-# Generate Makefile.in's from Makefile.am's and configure.in
-# (adding missing files that may be needed for proper operation)
-automake --add-missing --gnu
-
-# Generate config.h.in from acconfig.h and configure.in
-autoheader
-
-# Generate configure from configure.in and aclocal.m4
-autoconf
-
-# run ./config.status, if it exists, to create new Makefiles in subdirectories.
-# (Note that this only is useful if builddir == scrdir, otherwise the
-#  user may need to run config.status from their builddir(s) manually)
-test -x ./config.status && ./config.status
-
+gnulib-tool --import havelib
+autoreconf -i
